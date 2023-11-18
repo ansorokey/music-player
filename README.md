e66a52958d566320bc079583fd411edd35d84df3
ansorokey <ansorokey@gmail.com>
2023-11-17 20:24:43 -0500

    added comments, fixed nloadedmetadata

--- README.md
+++ README.md
@@ -5,8 +5,16 @@ This project was created by following [How To Make A Music Player Using HTML CSS
 
 ## Key Features and Technologies
 - JavaScript, HTML, CSS
+- DOM Manipulation
+- Event Listeners
+- Font Awesome
+- Webkit
+- Audio Controls
 
 ## How To Use
+Click play to begin the song. Click pause to pause the song. Clicking anywhere on the slider will play the song at that point.
 
 # Future Features
-- None currently planned
+- Functioning fast forward/rewind buttons
+- Read song title, artist, and other metadata from file (currently static)
+- add multiple songs to form a playlist
--- script.js
+++ script.js
@@ -2,30 +2,42 @@ let progress = document.querySelector('#progress');
 let song = document.querySelector('#song')
 let playBtn = document.querySelector('#play-pause')
 
-song.onloadmetadata = function() {
+// when the song's detail and file information is loaded:
+song.onloadedmetadata = function() {
+    console.log(song.metadata);
     progress.max = song.duration;
     progress.value = song.currentTime;
 }
 
 function playPause() {
+    // if the button is a pause button:
     if(playBtn.classList.contains('fa-pause')) {
-        song.pause();
+        song.pause(); // stop the song
+        // replace the fa icon witha play icon
         playBtn.classList.remove('fa-pause');
         playBtn.classList.add('fa-play');
     } else {
+        // do the opposite
         song.play();
         playBtn.classList.remove('fa-play');
         playBtn.classList.add('fa-pause');
     }
 }
 
+// Whenever the song is playing:
 if(song.play) {
+    // update the value of the progres bar every half second
     setInterval(() => {
         progress.value = song.currentTime;
     }, 500);
 }
+// how does this run repeatedly?
+// it isn't part of a function and should oly be read and passed once.
+// there's something about how a browser reads a script that I don't fully know
 
+// when the user changes the progress bar:
 progress.onchange = function() {
+    // play the song at that point
     song.play();
     song.currentTime = progress.value;
     playBtn.classList.remove('fa-play');
